cd292ea6fbfd7759d308fa05a2be037b6501b720
John Barksdale <jdbarks@gmail.com>
2019-01-09 22:43:40 -0600

    Created save() in journal.py

--- journal.py
+++ journal.py
@@ -1,13 +1,27 @@
+import os
+
+
 def load(name):
     # todo: populate from file if it exists
     return []
 
 
 def save(name, journal_data):
-    pass
+    filename = get_full_pathname(name)
+    print(f'..... saving to: {filename}')
+
+    with open(filename, 'w') as fout:
+        for entry in journal_data:
+            fout.write(entry + '\n')
+
+
+def get_full_pathname(name):
+    filename = os.path.abspath(os.path.join('.', 'journals', f'{name}.jrl'))
+    return filename
 
 
 def add_entry(text, journal_data):
     journal_data.append(text)
 
 # todo: could model as a class
+# todo: convert to pathlib
